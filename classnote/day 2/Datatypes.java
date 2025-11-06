71de21b6075d9025e838c787ca18837ea160cf58
chinu-am <hello123says@gmail.com>
2025-11-06 14:16:49 +0530

    Update Datatypes.java

--- classnote/day 2/Datatypes.java
+++ classnote/day 2/Datatypes.java
@@ -1,20 +1,27 @@
- public class Datatypes {
-  public static void main(String args[]){
-    System.out.println("This is printed by print statement");
-    System.out.println("Datatypes");
-    int num=20;
-    System.out.printf("This is Integer %d\n",num);
-    short numshort=365;
-    System.out.printf("This is Short Integer %d\n",numshort);
-    long numlong=3342;
-    System.out.printf("This is Long Integer %d\n",numlong);
-    float numdecimal=3.56f;
-    System.out.printf("this is Float %f\n",numdecimal);
-    double numdouble=44.32;
-    System.out.printf("This is Double %f\n",numdouble);
-    char charac='A';
-    System.out.printf("This is character %c\n",charac);
-   boolean sam= true;
-   System.out.printf("This is Boolean %b\n",sam);
-   
-    }}
+public class Datatypes {
+    public static void main(String args[]) {
+        System.out.println("This is printed by print statement");
+        System.out.println("Datatypes Example with New Values");
+
+        int num = 45;
+        System.out.printf("This is Integer %d\n", num);
+
+        short numshort = 1200;
+        System.out.printf("This is Short Integer %d\n", numshort);
+
+        long numlong = 9876543210L;
+        System.out.printf("This is Long Integer %d\n", numlong);
+
+        float numdecimal = 9.81f;
+        System.out.printf("This is Float %f\n", numdecimal);
+
+        double numdouble = 12345.6789;
+        System.out.printf("This is Double %f\n", numdouble);
+
+        char charac = 'Z';
+        System.out.printf("This is Character %c\n", charac);
+
+        boolean sam = false;
+        System.out.printf("This is Boolean %b\n", sam);
+    }
+}
